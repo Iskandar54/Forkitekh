1a31dcb0b05157bc1fd2b7ca90b1bfcc8cb93f69
Iskandar54 <iskandar5454@gmail.com>
2025-04-07 18:38:43 +0500

    Update database.py

--- tron_wallet_info/app/database.py
+++ tron_wallet_info/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:MiruMir22111998@localhost/test_tron"
+SQLALCHEMY_DATABASE_URL = "postgresql://name:password@localhost/db_name"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
